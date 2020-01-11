0c2efbdcdb860f18cb110e0fd12b79c396a6246f
Vasili Kutsko <terkin.vasja@gmail.com>
2020-01-12 00:10:00 +0300

    HW 4.4 Letters

---
+++ module04/4/Letters/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 65; i <= 90; i++) {
+            System.out.println(i + ": " + (char) i);
+        }
+        for (int i = 97; i <= 122; i++) {
+            System.out.println(i + ": " + (char) i);
+        }
+    }
+}
