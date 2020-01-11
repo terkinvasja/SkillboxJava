0f016e949c30ca09858e2e29fe8b4d2e2a5dc8f9
Vasili Kutsko <terkin.vasja@gmail.com>
2020-01-12 00:19:29 +0300

    HW 4.4 FIO

---
+++ module04/4/FIO/src/Loader.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Loader
+{
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println("Введите ФИО одной строкой в формате (Фамилия Имя Отчество)");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        String[] words = str.trim().split("\\s+");
+
+        System.out.println("Фамилия: " + words[0]);
+        System.out.println("Имя: " + words[1]);
+        System.out.println("Отчество: " + words[2]);
+    }
+}
