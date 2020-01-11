a8dc0925164b211abc8295c3e16b17fdadfeb4e7
Vasili Kutsko <terkin.vasja@gmail.com>
2020-01-11 23:46:44 +0300

    HW 4.2

---
+++ module04/2/MinMax/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(String.format("Byte: Min = %s, Max = %s", Byte.MIN_VALUE, Byte.MAX_VALUE));
+        System.out.println(String.format("Short: Min = %s, Max = %s", Short.MIN_VALUE, Short.MAX_VALUE));
+        System.out.println(String.format("Integer: Min = %s, Max = %s", Integer.MIN_VALUE, Integer.MAX_VALUE));
+        System.out.println(String.format("Long: Min = %s, Max = %s", Long.MIN_VALUE, Long.MAX_VALUE));
+        System.out.println(String.format("Float: Min = %s, Max = %s", Float.MIN_VALUE, Float.MAX_VALUE));
+        System.out.println(String.format("Double: Min = %s, Max = %s", Double.MIN_VALUE, Double.MAX_VALUE));
+    }
+}
