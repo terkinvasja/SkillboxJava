b13c7769dc81a8f048562e981686681ba2862a0a
Vasili Kutsko <terkin.vasja@gmail.com>
2020-01-13 23:09:25 +0300

    HW 4.4 FIO. Fixed comments.

--- module04/4/FIO/src/Loader.java
+++ module04/4/FIO/src/Loader.java
@@ -2,17 +2,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Loader
-{
-    public static void main(String[] args) throws IOException
-    {
+public class Loader {
+
+    private static boolean isDebugEnabled = false;
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Введите ФИО одной строкой в формате (Фамилия Имя Отчество)");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String str = reader.readLine();
-        String[] words = str.trim().split("\\s+");
+        String str = reader.readLine().trim();
 
-        System.out.println("Фамилия: " + words[0]);
-        System.out.println("Имя: " + words[1]);
-        System.out.println("Отчество: " + words[2]);
+        int firstSpace = str.indexOf(" ");
+        if (isDebugEnabled) {
+            debug("firstSpace = " + firstSpace);
+        }
+        String surname;
+        if (firstSpace != -1) {
+            surname = str.substring(0, firstSpace);
+        } else {
+            errorMessage();
+            return;
+        }
+
+        int secondSpace = str.indexOf(" ", firstSpace + 1);
+        if (isDebugEnabled) {
+            debug("secondSpace = " + secondSpace);
+        }
+        String name;
+        if (secondSpace != -1) {
+            name = str.substring(firstSpace + 1, secondSpace);
+        } else {
+            errorMessage();
+            return;
+        }
+
+        String patronymic = str.substring(secondSpace + 1);
+
+        System.out.println("Фамилия: " + surname);
+        System.out.println("Имя: " + name);
+        System.out.println("Отчество: " + patronymic);
+    }
+
+    private static void errorMessage() {
+        System.out.println("Введены некорректные данные");
+    }
+
+    private static void debug(String str) {
+        System.out.println(str);
     }
 }
