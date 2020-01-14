839dea2adb83bdaaec8c9d2a1e9fe4dce2fe1cd0
Vasili Kutsko <terkin.vasja@gmail.com>
2020-01-14 22:23:45 +0300

    HW 4.6 DateAndTime

---
+++ module04/6/DateAndTime/src/Main.java
@@ -0,0 +1,22 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - E");
+
+        int toDayYear = Calendar.getInstance().get(Calendar.YEAR);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1987, Calendar.APRIL, 02, 02, 00, 00);
+
+        int years = toDayYear - calendar.get(Calendar.YEAR);
+        for (int i = 0; i <= years; i++) {
+            Date date = calendar.getTime();
+            System.out.println(String.format("%s - %s", i, dateFormat.format(date)));
+            calendar.add(Calendar.YEAR, 1);
+        }
+    }
+}
